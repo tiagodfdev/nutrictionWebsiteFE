6f454c702a35171057d819c71ee8eb71a0aa4bfd
Tiago Freire <tiagodfdev@gmail.com>
2022-06-19 17:23:27 -0300

    update configs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
-import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { Provider } from 'react-redux';
+import Layout from '../components/Layout/Layout';
+import theme from '../styles/theme';
+import { store } from '../redux/store';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+    <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <Layout>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
+    </Provider>
+    </>
+  );
 }
 
 export default MyApp;
