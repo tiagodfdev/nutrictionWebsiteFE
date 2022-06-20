41aacfe09cfe9311a6dc694932a3633e5452e3b9
Tiago Freire <tiagodfdev@gmail.com>
2022-06-20 00:02:18 -0300

    adding styles

---
+++ styles/theme.tsx
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#204907',
+    },
+    secondary: {
+      main: '#E8871E',
+    },
+  },
+});
+
+export default theme;
