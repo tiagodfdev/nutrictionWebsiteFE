086b822dd746aaaff6948f954e5de4b6215fbe28
Tiago Freire <tiagodfdev@gmail.com>
2022-10-07 20:39:45 -0300

    SEO search advance

--- components/Pages/HomeContent.tsx
+++ components/Pages/HomeContent.tsx
@@ -27,17 +27,17 @@ function HomeContent(props:IProps) {
             {
               "@context": "https://schema.org",
               "@type": "WebSite",
-              "name": "Informações Nutricionais HomePage",
               "url": "https://nutriction-website-fe.vercel.app/",
               "potentialAction": {
                 "@type": "SearchAction",
                 "target": {
                   "@type": "EntryPoint",
-                  "urlTemplate": "https://nutriction-website-fe.vercel.app/item/{search-term}"
+                  "urlTemplate": "https://nutriction-website-fe.vercel.app/item/{search_term_string}"
                 },
-                "query-input": "required name=search-term"
+                "query-input": "required name=search_term_string"
               }
             }
+        
           `}
         </script>
       </Head>
